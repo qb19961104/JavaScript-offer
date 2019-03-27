c5b5daab8b586c6af01b2bb5dd80ae64b2aa09d5
qiaobing <qb20150806@163.com>
2019-03-27 16:59:59 +0800

    冒泡排序

---
+++ test06.js
@@ -0,0 +1,14 @@
+function BubbleSort(arr){
+    for(var i=0;i<arr.length;i++){
+        for(var j=0;j<arr.length;j++){
+            if(arr[j]>arr[j+1]){
+                var Temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1]= Temp;
+            }
+        } 
+    }
+    return arr;
+}
+var arr=[8,4,6,9,15,47,10,48];
+console.log(BubbleSort(arr));
